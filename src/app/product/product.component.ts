74c277abf78b3377d1ebc86ea9c1b4e86bb456d6
yangqian <qian.yang2@thoughtworks.com>
2020-11-15 20:58:29 +0800

    [yangqian] feat: click product to product detail in list.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Product} from '../product';
+import {NzMessageService} from 'ng-zorro-antd/message';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -9,9 +11,13 @@ import {Product} from '../product';
 export class ProductComponent implements OnInit {
 
   @Input() product: Product;
-  constructor() { }
+  constructor(private message: NzMessageService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  productDetail(): void {
+    this.router.navigate([`products/${this.product.id}`]);
+  }
 }
