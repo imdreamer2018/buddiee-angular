f255cb2f6dd78cc46501d4a4605968f3149f0e2b
yangqian <qian.yang2@thoughtworks.com>
2020-11-15 16:03:16 +0800

    [yangqian] fix: base url.

--- src/app/api/products.ts
+++ src/app/api/products.ts
@@ -1,29 +1,22 @@
-import {GET, Query, RebirthHttpClient, Any} from '@ng-zorro/rebirth-http';
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
 import {Page} from '../page';
 import {Product} from '../product';
+import {Observable} from 'rxjs';
 
-@Injectable()
-export class ProductsApi extends RebirthHttpClient {
-  static BASE_URL: 'http://127.0.0.1:8080';
-  constructor(http: HttpClient) {
-    super(http);
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsApi {
+  BASE_URL: 'http://127.0.0.1:8080';
+  constructor(private http: HttpClient) {
   }
 
-  @GET(`${ProductsApi.BASE_URL}/products`)
-  getProducts(
-    @Query('pageNumber') pageNumber: number,
-    @Query('pageSize') pageSize: number
-  ): Observable<Page<Product[]>> {
-    return Any;
+  getProducts(pageNumber: number, pageSize: number): Observable<Page<Product[]>> {
+      return this.http.get<Page<Product[]>>(`http://127.0.0.1:8080/products?pageNumber=${pageNumber}&pageSize=${pageSize}`);
   }
 
-  @GET(`${ProductsApi.BASE_URL}/product/:id`)
-  getProduct(
-    @Query('id') id: number,
-  ): Observable<Product> {
-    return Any;
+  getProduct(id: number): Observable<Product> {
+    return this.http.get<Product>(`http://127.0.0.1:8080/products/${id}`);
   }
 }
