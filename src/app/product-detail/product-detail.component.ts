2b783a5068ad40ce54e7631f78362b3473bdcfaa
yangqian <qian.yang2@thoughtworks.com>
2020-11-15 16:06:23 +0800

    [yangqian] feat: products list and detail.

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from '../product';
+import {ActivatedRoute} from '@angular/router';
+import {Store} from '@ngrx/store';
+import {selectorProduct} from '../store/selectors/products.selector';
+import {map, mergeMap} from 'rxjs/operators';
+import {loadProduct} from '../store/actions/products.action';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +12,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-
-  constructor() { }
+  product?: Product;
+  constructor(private route: ActivatedRoute,
+              private store: Store) { }
 
   ngOnInit(): void {
+    this.store.select(selectorProduct).subscribe(product => this.product = product);
+    this.route.paramMap.pipe(
+      map(paramMap => Number(paramMap.get('id'))),
+      map(id => id)
+    ).subscribe(id => {
+      this.store.dispatch(loadProduct({id}));
+    });
   }
 
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Page} from '../page';
+import {Product} from '../product';
+import {Router} from '@angular/router';
+import {Store} from '@ngrx/store';
+import {selectorProducts} from '../store/selectors/products.selector';
+import {loadProducts} from '../store/actions/products.action';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +12,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  products?: Page<Product[]>;
+  currentPage: number;
 
-  constructor() { }
+  constructor(private router: Router,
+              private store: Store) { }
 
   ngOnInit(): void {
+    this.currentPage = 1;
+    this.store.select(selectorProducts).subscribe(products => this.products = products);
+    this.store.dispatch(loadProducts({pageNumber: this.currentPage}));
   }
 
 }
