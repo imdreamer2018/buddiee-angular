ddf2582d8fe41b94fc010b2eee05ab72500b15de
yangqian <qian.yang2@thoughtworks.com>
2020-11-15 20:09:22 +0800

    [yangqian] feat: get file base64.

---
+++ src/app/util/geFileBase64.ts
@@ -0,0 +1,8 @@
+export const getFileBase64 = (file: File): Promise<string | ArrayBuffer | null> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
+};
